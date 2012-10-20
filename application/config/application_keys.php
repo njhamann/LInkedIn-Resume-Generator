d53d896c7427ef39e93cd7943069d871ae026f44
Noah Hamann <njhamann@gmail.com>
2012-10-20 19:17:58 +0000

    -changed config file

--- application/config/application_keys.php
+++ application/config/application_keys.php
@@ -1,6 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
     $config['LINKEDIN_KEYS'] = array(
-        'appKey'       => 'ewx5138o17am',
-	    'appSecret'    => 'nNhK5FKH1aGN6gC7',
 	    'callbackUrl'  => NULL 
     );
